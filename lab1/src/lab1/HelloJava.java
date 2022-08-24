3add0760f72fb9b9cbfcb6de8149e2079f00344b
olialemyk <olha.lemyk.ki.2020@lpnu.ua>
2022-08-24 18:26:55 +0300

    lab1

---
+++ lab1/src/lab1/HelloJava.java
@@ -0,0 +1,14 @@
+package lab1;
+
+public class HelloJava {
+	
+	
+	public static void main(String[] args)
+	{
+	String message = new String();
+	message = "Lemyk Olia KI-36";
+	System.out.println(message);
+	}
+	
+
+}
